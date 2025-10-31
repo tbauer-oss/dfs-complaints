f5371c9445255356b829c1d6d751858631b3737b
tbauer-oss <tbauer.mail@googlemail.com>
2025-10-31 08:54:27 +0100

    Update mail.js

--- api/_lib/mail.js
+++ api/_lib/mail.js
@@ -10,7 +10,7 @@ export const mailer = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT || 587),
   secure: toBool(process.env.SMTP_SECURE),
-  auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS }
+  auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
   authMethod: 'LOGIN',
 });
 
