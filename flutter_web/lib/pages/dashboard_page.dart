4f2bdd1385d302a996d52a087767f24123c7f19f
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 11:56:49 +0100

    Update dashboard_page.dart

--- flutter_web/lib/pages/dashboard_page.dart
+++ flutter_web/lib/pages/dashboard_page.dart
@@ -726,6 +726,7 @@ class _FancyTile extends StatelessWidget {
   }
 }
 
+// ---------------- In-App PDF Viewer (pdf.js) ----------------
 class PdfInAppPage extends StatefulWidget {
   final String url;
   final String title;
@@ -742,20 +743,17 @@ class _PdfInAppPageState extends State<PdfInAppPage> {
   void initState() {
     super.initState();
 
-    // 1) Proxy-URL (Same-Origin)
-    final origin = html.window.location.origin; // z.B. https://dfs-complaints-web.vercel.app
-    final proxied = '$origin/api/pdf-proxy?url=${Uri.encodeComponent(widget.url)}';
-
-    // 2) pdf.js Viewer mit Suche/Zoom – Datei param auf PROXY zeigen lassen
-    final viewer = 'https://mozilla.github.io/pdf.js/web/viewer.html'
-        '?file=${Uri.encodeComponent(proxied)}#zoom=page-width&pagemode=none';
+    // pdf.js-Viewer mit Toolbar & Suche
+    final pdfUrl = Uri.encodeComponent(widget.url);
+    final src = 'https://mozilla.github.io/pdf.js/web/viewer.html'
+        '?file=$pdfUrl#zoom=page-width&pagemode=none';
 
     _viewType = 'pdfjs-${DateTime.now().millisecondsSinceEpoch}';
 
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory(_viewType, (int viewId) {
       final frame = html.IFrameElement()
-        ..src = viewer
+        ..src = src
         ..style.border = '0'
         ..style.width = '100%'
         ..style.height = '100%';
