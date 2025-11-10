e0d44928b70b476ab0ceb9438024744e61626b1a
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 18:01:29 +0100

    Update privacy_consent.dart

--- dfs_mobile/lib/widgets/privacy_consent.dart
+++ dfs_mobile/lib/widgets/privacy_consent.dart
@@ -1,8 +1,8 @@
 // lib/widgets/privacy_consent.dart
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'web_compat/html_stub.dart'
-  if (dart.library.html) 'web_compat/html_web.dart' as html;
+import 'package:dfs_mobile/web_compat/html_stub.dart'
+  if (dart.library.html) 'package:dfs_mobile/web_compat/html_web.dart' as html;
 import '../constants.dart';
 
 class PrivacyConsent extends StatelessWidget {
