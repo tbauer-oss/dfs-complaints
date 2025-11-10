bfa67548e65c58798cfb1ef4a4522873e8502d8f
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 17:58:29 +0100

    Update app_prefs.dart

--- dfs_mobile/lib/services/app_prefs.dart
+++ dfs_mobile/lib/services/app_prefs.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'web_compat/html_stub.dart'
-  if (dart.library.html) 'web_compat/html_web.dart' as html;
+import 'package:dfs_mobile/web_compat/html_stub.dart'
+  if (dart.library.html) 'package:dfs_mobile/web_compat/html_web.dart' as html;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
