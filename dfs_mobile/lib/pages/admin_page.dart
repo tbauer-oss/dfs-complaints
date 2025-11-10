506bbbbc25f87c37a01527768cb40f7308261695
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 17:59:01 +0100

    Update admin_page.dart

--- dfs_mobile/lib/pages/admin_page.dart
+++ dfs_mobile/lib/pages/admin_page.dart
@@ -1,8 +1,8 @@
 // lib/pages/admin_page.dart
 import 'dart:convert';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'web_compat/html_stub.dart'
-  if (dart.library.html) 'web_compat/html_web.dart' as html;
+import 'package:dfs_mobile/web_compat/html_stub.dart'
+  if (dart.library.html) 'package:dfs_mobile/web_compat/html_web.dart' as html;
 import 'package:flutter/material.dart';
 import '../api/client.dart';
 import '../widgets/legal_footer.dart';
