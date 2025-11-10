d89fcf2abb833ac531d212619201a906184b85f6
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 16:54:26 +0100

    Update my_complaints_page.dart

--- dfs_mobile/lib/pages/my_complaints_page.dart
+++ dfs_mobile/lib/pages/my_complaints_page.dart
@@ -1,6 +1,8 @@
 // lib/pages/my_complaints_page.dart
 import 'dart:async';
-import 'dart:html' as html; // nur Web – für Link-Öffnen & mailto
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'web_compat/html_stub.dart'
+  if (dart.library.html) 'web_compat/html_web.dart' as html;
 import 'package:flutter/material.dart';
 import '../api/client.dart';
 import '../models/complaint.dart';
