a1fb0462239130a25535fe2f22addac04ecd8ceb
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 17:57:57 +0100

    Update client.dart

--- dfs_mobile/lib/api/client.dart
+++ dfs_mobile/lib/api/client.dart
@@ -1,8 +1,8 @@
 // lib/api/client.dart
 import 'dart:convert';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'web_compat/html_stub.dart';
-  if (dart.library.html) 'web_compat/html_web.dart' as html;
+import 'package:dfs_mobile/web_compat/html_stub.dart'
+  if (dart.library.html) 'package:dfs_mobile/web_compat/html_web.dart' as html;
 import 'package:http/http.dart' as http;
 import '../models/complaint.dart';
 
