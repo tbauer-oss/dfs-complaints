f0a5ff49f6780871e1e1743d75ae0876d477baa1
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-10 16:16:10 +0100

    Update build.gradle.kts

--- dfs_mobile/android/app/build.gradle.kts
+++ dfs_mobile/android/app/build.gradle.kts
@@ -20,14 +20,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "de.dfs_diamon.dfs_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId = "de.dfs_diamon.dfs_complaints"
+        minSdkVersion = 21
+        targetSdkVersion = 34
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
@@ -42,3 +39,4 @@ android {
 flutter {
     source = "../.."
 }
+
