851c14d1f8e6ee621f0c669961ee341c6583f9cc
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-12 15:39:32 +0100

    Update build.gradle.kts

--- dfs_mobile/android/build.gradle.kts
+++ dfs_mobile/android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 allprojects {
@@ -26,4 +26,5 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
 
