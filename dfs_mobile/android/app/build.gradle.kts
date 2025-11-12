8d5e04cc85c3182a67ef4781501627034d1cef96
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-12 15:23:47 +0100

    Update build.gradle.kts

--- dfs_mobile/android/app/build.gradle.kts
+++ dfs_mobile/android/app/build.gradle.kts
@@ -67,7 +67,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
     implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
     implementation("com.google.firebase:firebase-messaging")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
+
 
 
