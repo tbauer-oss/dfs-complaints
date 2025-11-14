10d81ff3ecb0ae822171f66a6ff5e39e94d94ff3
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-14 10:58:37 +0100

    Update admin_page.dart

--- flutter_web/lib/pages/admin_page.dart
+++ flutter_web/lib/pages/admin_page.dart
@@ -995,7 +995,7 @@ Widget _buildUsersPanel() {
                 icon: const Icon(Icons.refresh),
               ),
             ]),
-
+          ]
 
             // >>> NEU: Filterzeile direkt unter der Überschrift <<<
             const SizedBox(height: 8),
