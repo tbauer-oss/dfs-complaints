bda33a7651be01c89dc7519e97875dff2a445aae
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-14 19:46:39 +0100

    Update rep_dashboard_page.dart

--- dfs_mobile/lib/pages/rep_dashboard_page.dart
+++ dfs_mobile/lib/pages/rep_dashboard_page.dart
@@ -589,6 +589,7 @@ class _RepDashboardPageState extends State<RepDashboardPage> {
               try {
                 await widget.api.ensureRepSession();
                 await widget.api.repCreateCustomer(payload);
+                FocusScope.of(ctx).unfocus();
                 FocusManager.instance.primaryFocus?.unfocus();
                 if (Navigator.of(ctx).canPop()) {
                   Navigator.of(ctx).pop(true);
