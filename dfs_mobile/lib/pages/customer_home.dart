4855101daee682981b0c44e6b5bde1da709df519
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-24 20:50:54 +0100

    Add dedicated complaint wizard entry for customer area

--- dfs_mobile/lib/pages/customer_home.dart
+++ dfs_mobile/lib/pages/customer_home.dart
@@ -207,6 +207,23 @@ class CustomerHomePage extends StatelessWidget {
                                         SizedBox(
                                           width: targetWidth,
                                           child: buildAction(
+                                            icon: Icons.auto_awesome_outlined,
+                                            label: t.complaintWizardTile ?? 'Reklamations-Assistent',
+                                            onTap: () {
+                                              Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                  builder: (_) => ComplaintFormPage(
+                                                    api: api,
+                                                    wizardMode: true,
+                                                  ),
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: targetWidth,
+                                          child: buildAction(
                                             icon: Icons.list_alt,
                                             label: t.myComplaints,
                                             onTap: () {
