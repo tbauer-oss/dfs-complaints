a04576c9a2a76b7c796cbacf3969381a5f35683d
tbauer-oss <tbauer.mail@googlemail.com>
2025-11-24 13:35:35 +0100

    Add files via upload

---
+++ dfs_mobile/android/app/build.gradle.kts
@@ -0,0 +1,77 @@
+// android/app/build.gradle.kts
+
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+android {
+    namespace = "de.dfs_diamon.dfs_complaints"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "de.dfs_diamon.dfs_complaints" // <- deine Paketkennung
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+
+    signingConfigs {
+        create("release") {
+            val keystoreProps = Properties()
+            val kpFile = rootProject.file("key.properties")
+            if (kpFile.exists()) {
+                keystoreProps.load(FileInputStream(kpFile))
+                storeFile = file(keystoreProps["storeFile"] as String)
+                storePassword = keystoreProps["storePassword"] as String
+                keyAlias = keystoreProps["keyAlias"] as String
+                keyPassword = keystoreProps["keyPassword"] as String
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+            // ProGuard bei Bedarf:
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+            // optional debug-spezifisches
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
+
+
+
+
+
