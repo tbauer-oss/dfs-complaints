11d650a15d6fed9c1de180dc25162817cfc085d4
tbauer-oss <tbauer.mail@googlemail.com>
2025-12-06 12:54:19 +0100

    Add complaint list tile rendering

--- flutter_web/lib/pages/admin_page.dart
+++ flutter_web/lib/pages/admin_page.dart
@@ -415,7 +415,8 @@ class _AdminPageState extends State<AdminPage> {
     _persistCustomerContactSeen();
   }
 
-  void _loadRoleTileVisibility({Map<String, dynamic>? stored}) {
+  bool _loadRoleTileVisibility({Map<String, dynamic>? stored}) {
+    var addedDefaults = false;
     final rawData = stored;
     if (rawData != null) {
       rawData.forEach((key, value) {
@@ -432,9 +433,11 @@ class _AdminPageState extends State<AdminPage> {
         if (tiles.add(tile)) changed = true;
       }
       if (changed) {
-        _persistRoleTileVisibility(syncRemote: false);
+        addedDefaults = true;
       }
     });
+
+    return addedDefaults;
   }
 
   String? _normalizeTilePermission(Object? raw) {
@@ -5070,9 +5073,13 @@ class _AdminPageState extends State<AdminPage> {
 
       final remoteTiles = config['roleTileVisibility'];
       if (remoteTiles is Map<String, dynamic>) {
-        setState(() => _loadRoleTileVisibility(stored: remoteTiles));
+        var updatedVisibility = false;
+        setState(() => updatedVisibility = _loadRoleTileVisibility(stored: remoteTiles));
+        if (updatedVisibility) {
+          _persistRoleTileVisibility(syncRemote: true);
+        }
         _filterMenuSectionsForRole();
-        _persistRoleTileVisibility(syncRemote: false);
+        _ensureMenuTilePresent('complaintList');
       }
 
       final remoteLayout = config['menuLayout'];
@@ -5985,6 +5992,19 @@ class _AdminPageState extends State<AdminPage> {
           count: _allComplaints.length,
           compact: compact,
           onTap: isPreview ? () {} : () => setState(() => _view = _AdminView.all),
+          actionLabel: resolvedActionLabel,
+          actionIcon: resolvedActionIcon,
+          onActionTap: onActionTap,
+        );
+      case 'complaintList':
+        return AdminTilePro(
+          label: 'Reklamationsliste',
+          subtitle: 'Übersicht & Export',
+          icon: Icons.table_view_outlined,
+          colorA: AdminPalette.blueA,
+          colorB: AdminPalette.blueB,
+          compact: compact,
+          onTap: isPreview ? () {} : () => setState(() => _view = _AdminView.complaintList),
           actionLabel: resolvedActionLabel,
           actionIcon: resolvedActionIcon,
           onActionTap: onActionTap,
