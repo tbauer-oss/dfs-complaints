2a0a928d4a24f90971ca308c4f8d7df5a4882c36
tbauer-oss <tbauer.mail@googlemail.com>
2025-12-10 19:10:11 +0100

    Restore custom CORS allow header support

--- api/_options.js
+++ api/_options.js
@@ -1,9 +1,10 @@
 // api/_options.js
 export const config = { runtime: 'nodejs' };
 
-import { setCors } from './_lib/cors.js';
+import { setCors, handlePreflight } from './_lib/cors.js';
 
 export default function handler(req, res) {
   setCors(req, res);
+  if (handlePreflight(req, res)) return;
   return res.status(204).end();
 }
