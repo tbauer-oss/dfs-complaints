1dde7b6d7a90a094d86d3df48c3c3be54730b2fb
tbauer-oss <tbauer.mail@googlemail.com>
2025-12-16 00:17:45 +0100

    Update refresh.js

--- api/rep/refresh.js
+++ api/rep/refresh.js
@@ -1,5 +1,5 @@
 // api/rep/refresh.js
-export const config = { runtime: 'nodejs.x' };
+export const config = { runtime: 'nodejs' };
 
 import jwt from 'jsonwebtoken';
 import { setCors } from '../_lib/cors.js';
