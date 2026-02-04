700054c7b1a2c2ac6e63697e120399d4d31d7716
tbauer-oss <tbauer.mail@googlemail.com>
2026-02-04 08:05:15 +0100

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -1,9 +1,10 @@
 // api/[...path].js - catch-all OPTIONS handler for Vercel
 export const config = { runtime: 'nodejs' };
 
-import { bad } from './_lib/http.js';
+import { bad, withCors } from './_lib/http.js';
 
 export default function handler(req, res) {
+  withCors(req, res);
   if (req.method === 'OPTIONS') {
     return res.status(204).end();
   }
