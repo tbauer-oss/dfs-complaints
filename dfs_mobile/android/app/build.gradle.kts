a4cbd05a85d91f1320d9cc798c57db6ea1909657
tbauer-oss <tbauer.mail@googlemail.com>
2026-02-06 16:55:15 +0100

    Update build.gradle.kts

--- dfs_mobile/android/app/build.gradle.kts
+++ dfs_mobile/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "de.dfs_diamon.dfs_complaints" // <- deine Paketkennung
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 2
         versionName = "1.0.1"
