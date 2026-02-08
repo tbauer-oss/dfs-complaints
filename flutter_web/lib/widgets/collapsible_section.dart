1d585240ac0917f1f171299b9ba61a2ad1ad552d
tbauer-oss <tbauer.mail@googlemail.com>
2026-02-08 15:28:44 +0100

    Update collapsible_section.dart

--- flutter_web/lib/widgets/collapsible_section.dart
+++ flutter_web/lib/widgets/collapsible_section.dart
@@ -65,7 +65,7 @@ class CollapsibleSection extends StatelessWidget {
         const Divider(height: 16),
         AnimatedCrossFade(
           firstChild: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 18), // EdgeInsets.fromLTRB(left, top, right, bottom)
+            padding: const EdgeInsets.fromLTRB(10, 10, 14, 18), // EdgeInsets.fromLTRB(left, top, right, bottom)
             child: child,
           ),
           secondChild: const SizedBox.shrink(),
